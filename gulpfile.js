f01f3da594f0472750e4c782dd3d7e0c025b78a7
AleksandraWiktorzak <44510430+AleksandraWiktorzak@users.noreply.github.com>
2019-06-12 10:10:54 +0200

    Pierwszy commit
    Gotowe wtyczki z gulpa.
    Footer zrobiony, przycisk z animacją i delikatnym przejściem na górę strony.
    Problem ze zdjęciami, nie wczytują się na stronie mimo dobrej ścieżki (404 not found).
    Podczas instalowania wtyczek może wyskoczyć error związany z node-sass. Trzeba to zainstalować ręcznie.

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+var juck = require('gulp-nunjucks-render');
+var wait = require('gulp-wait');
+var min = require('gulp-imagemin');
+
+gulp.task('img', function () {
+    gulp.src('./src/images/**/*')
+      .pipe(min())
+      .pipe(gulp.dest('./web/images'))
+  });
+
+gulp.task('juck', function() {
+    return gulp.src(['./src/templates/*.html'])
+      .pipe(juck({
+        path: ['./src/templates']
+      }))
+      .pipe(gulp.dest('./web'))
+  });
+
+gulp.task('reload', function(){
+    browserSync.reload()
+});
+
+gulp.task('serve', function () {
+    browserSync.init({
+        server: './web'
+    });  
+    gulp.watch('./src/templates/index.html', ['reload']);
+    gulp.watch('./src/sass/*.scss',['sass']);
+    gulp.watch('./src/templates/components/*.html',['juck']);
+    gulp.watch('./src/templates/components/*.html',['reload'])
+    
+})
+gulp.task('sass', function() {
+     return gulp.src('./src/sass/style.scss')
+     .pipe(wait(500))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./web/css'))
+    .pipe(browserSync.stream());
+});
+gulp.task('build', ['serve', 'reload', 'sass','juck', 'img']);
+gulp.task('default',['build'])
