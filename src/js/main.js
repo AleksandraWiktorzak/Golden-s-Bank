5ef92db81391cfa276987c61ef43c2ca9fd0ef38
AleksandraWiktorzak <44510430+AleksandraWiktorzak@users.noreply.github.com>
2019-06-16 20:16:45 +0200

    Drugi slider zrobiony

--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,46 @@
+// slider z wiekiem/////////////////////////////////////////
 var $range = $('.range');
 
 $range.each(function () {
 
-    var $thumb = $(this).next('.age');
-    var max = this.max;
-    //   parseInt(this.max, 10);
-    var tw = 180; // Thumb width. See CSS
+    var $age = $(this).next('.age');
+    let max = this.max;
+    let tw = 180; 
 
     $(this).on('input input.range', function () {
 
-        var w = $(this).width();
-        var val = this.value;
-        // parseInt(this.value, 10);
-        var txt = val;
-        var xPX = (val * 4) * (w - tw) / max * 0.7; // Position in PX
-        // var xPC = xPX * 100 / w;     // Position in % (if ever needed)
+        let w = $(this).width();
+        let val = this.value;
+        let txt = val;
+        let xPX = (val * 4) * (w - tw) / max * 0.7;
 
-        $thumb.css({ left: xPX - 120 }).attr("data-val", txt);
+        $age.css({ left: xPX - 120 }).attr("data-val", txt);
 
     });
 
 });
+$range.trigger('input.range');
+$(window).on("resize", () => $range.trigger("input.range"));
+////////////////////////////////////////////////////////////
+var $rangeP = $('.rangeP');
 
-$range.trigger('input.range'); // Calc on load
-$(window).on("resize", () => $range.trigger("input.range")); // and on resize
+$rangeP.each(function () {
+
+    var $period = $(this).next('.period');
+    let max = this.max;
+    let tw = 60; 
+
+    $(this).on('input input.rangeP', function () {
+
+        let w = $(this).width();
+        let val = this.value;
+        let txt = "do " + val + " lat";
+        let xPX = (val * 2.5) * (w - tw) / max * 0.5;
+
+        $period.css({ left: xPX -165 }).attr("data-val", txt);
+
+    });
+
+});
+$range.trigger('input.rangeP');
+$(window).on("resize", () => $range.trigger("input.rangeP"));
